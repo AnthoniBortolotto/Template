40b3dc917a7cc8baaab5d17ed73c11cbab39507a
anthoni Bortolotto Martinelli <antho@outlook.com.br>
2020-12-23 17:11:05 -0300

    Tá tudo funcionando no começo(.ts)

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import func from "./func";
+const twitter: string = "shahzebdev";
+const days: number = 3;
+console.log(`My twitter is @${twitter} and it took me ${days} days to write this article`);
+console.log("mudou");
+func();
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+
+const DEV = true;
+
+module.exports = {
+    entry: [
+        "./src/func.ts",
+        "./src/index.ts"
+    ],
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    module: {
+        rules: [{
+            test: /\.ts?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    },
+    watch: true,
+    watchOptions: {
+        ignored: /node_modules/
+    },
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    mode: DEV ? "development" : "production",
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 9000,
+        watchOptions: {
+            ignored: [
+                path.resolve(__dirname, "dist"),
+                path.resolve(__dirname, "node_modules")
+            ]
+        }
+    },
+};
