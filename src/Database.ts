f867dc90812da7726e2c7b41bd664970317ae907
anthoni Bortolotto Martinelli <antho@outlook.com.br>
2020-12-24 13:52:01 -0300

    Criação parcial do método montar tabela

--- src/Database.ts
+++ src/Database.ts
@@ -1,5 +1,4 @@
 import Produto from "Produto.ts";
-
 let db:IDBDatabase;
 const nomeDB = "Banco";
 const nomeLoja = "Produtos";
@@ -9,14 +8,12 @@ export function CriarDB()
     if(window.indexedDB)
     {
         const request = window.indexedDB.open(nomeDB, 2);
-        request.onerror = (event) =>
-        {
-            console.log('erro na criação', event);
-        }
+        request.onerror = (event) =>  console.log('erro na criação', event);
         request.onsuccess = (event) =>
         {
             db = request.result;
             console.log("sucesso na criação", event, db);
+            montarTabela();
         }
         request.onupgradeneeded = (event:any) =>
         {
@@ -40,4 +37,27 @@ export function Adicionar(produto:Produto)
     loja.add(produto);
     transaction.oncomplete = () => console.log('adicionado');
     transaction.onerror = () => console.log('houve Erro');
+}
+
+export function montarTabela()
+{
+    let exibTransact = db.transaction(nomeLoja);
+    let loja = exibTransact.objectStore(nomeLoja);
+
+    loja.openCursor().onsuccess = (event:any) => {
+        const cursor = <IDBDatabase>event.target.result;
+        console.log(cursor);
+        // if(cursor)
+        // {
+        //     let tabela = document.querySelector('#corpo-tabela');
+        //     tabela.innerHTML = `
+        //     <tr>
+        //     <td>${cursor.}</td>
+        //     <td></td>
+        //     <td></td>
+        //     <td></td>
+        //     <td></td>
+        // </tr>`
+        // }
+    }
 }
